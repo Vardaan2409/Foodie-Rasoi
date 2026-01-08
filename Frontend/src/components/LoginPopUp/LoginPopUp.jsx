ed9f4939322fafcd9bac81b21aaabb7a8899ea25
Vardaan Madhisiya <madhisiya.vardan24@gmail.com>
2026-01-09 02:13:47 +0530

    change

--- Frontend/src/components/LoginPopUp/LoginPopUp.jsx
+++ Frontend/src/components/LoginPopUp/LoginPopUp.jsx
@@ -4,12 +4,12 @@ import { assets } from '../../assets/assets';
 import { StoreContext } from '../context/StoreContext';
 import axios from "axios";
 
-const LoginPopUp = ({setShowLogin}) => {
+const LoginPopUp = ({ setShowLogin }) => {
 
-    const {url, setToken} = useContext(StoreContext);
+    const { url, setToken } = useContext(StoreContext);
 
     const [currState, setCurrState] = useState("Sign Up");
-    const [data, setData] = useState ({
+    const [data, setData] = useState({
         name: "",
         email: "",
         password: ""
@@ -18,7 +18,7 @@ const LoginPopUp = ({setShowLogin}) => {
     const onChangeHandler = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        setData(data => ({...data, [name]: value}));
+        setData(data => ({ ...data, [name]: value }));
     };
 
     const onLogin = async (event) => {
@@ -44,32 +44,32 @@ const LoginPopUp = ({setShowLogin}) => {
 
     }
 
-  return (
-    <div className='login-popup'>
-        <form onSubmit={onLogin} className="login-popup-container">
-            <div className="login-popup-title">
-                <h2>{currState}</h2>
-                <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
-            </div>
-            <div className="login-popup-inputs">
-                {currState==="Login" ? <></> : <input name="name" onChange={onChangeHandler} value={data.name} type="text" placeholder='Your name' required /> }
-                
-                <input name="email" onChange={onChangeHandler} value={data.email} type="email" placeholder='Your email' required />
-                <input name="password" onChange={onChangeHandler} value={data.password} type="password" placeholder='Password' required />
-            </div>
-            <button type="submit">{currState==="Sign Up" ? "Create Account" : "Login"}</button>
-            <div className="login-popup-condition">
-                <input type="checkbox" required/>
-                <p>By continuing, i agree to the terms of use & privacy policy.</p>
-            </div>
-            {
-                currState==="Login"
-                ? <p>Create a new account? <span onClick={() => setCurrState("Sign Up")}>Click here</span></p>
-                : <p>Already have an account? <span onClick={() => setCurrState("Login")}>Login here</span></p>
-            }
-        </form>
-    </div>
-  )
+    return (
+        <div className='login-popup'>
+            <form onSubmit={onLogin} className="login-popup-container">
+                <div className="login-popup-title">
+                    <h2>{currState}</h2>
+                    <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
+                </div>
+                <div className="login-popup-inputs">
+                    {currState === "Login" ? <></> : <input name="name" onChange={onChangeHandler} value={data.name} type="text" placeholder='Your name' required />}
+
+                    <input name="email" onChange={onChangeHandler} value={data.email} type="email" placeholder='Your email' required />
+                    <input name="password" onChange={onChangeHandler} value={data.password} type="password" placeholder='Password' required />
+                </div>
+                <button type="submit">{currState === "Sign Up" ? "Create Account" : "Login"}</button>
+                <div className="login-popup-condition">
+                    <input type="checkbox" required />
+                    <p>By continuing, i agree to the terms of use & privacy policy.</p>
+                </div>
+                {
+                    currState === "Login"
+                        ? <p>Create a new account? <span onClick={() => setCurrState("Sign Up")}>Click here</span></p>
+                        : <p>Already have an account? <span onClick={() => setCurrState("Login")}>Login here</span></p>
+                }
+            </form>
+        </div>
+    )
 }
 
 export default LoginPopUp
